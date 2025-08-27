cc6c855d4adb3dc0f4b42c004f36053e0c14d219
Aditya Kushwaha <adityakushwahaindia@gmail.com>
2025-08-27 18:34:46 +0530

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const { connectDB } = require("./connect");
 const urlRoute = require("./routes/url");
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+app.use(cors());
 app.use(express.json());
 
 connectDB(process.env.MONGO_URL)
